f68ccbb4578ca8b58a57e1cf2fafeda31872ea9a
Ryan Jones <ryan.j.jones10@gmail.com>
2016-04-20 22:03:20 -0700

    got it working for receiver

--- BeaconReceiver/ViewController.swift
+++ BeaconReceiver/ViewController.swift
@@ -8,89 +8,40 @@
 
 import UIKit
 import CoreLocation
-import CoreBluetooth
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     
     @IBOutlet weak var Found: UILabel!
     @IBOutlet weak var UUID: UILabel!
-    @IBOutlet weak var Major: UILabel!
     @IBOutlet weak var Minor: UILabel!
     @IBOutlet weak var Accuracy: UILabel!
     @IBOutlet weak var Distance: UILabel!
     @IBOutlet weak var RSSI: UILabel!
+    @IBOutlet var major: UILabel!
     
-    let uuidObj = NSUUID(UUIDString: "66dae67d-22e2-466b-b7d6-7093d52ceeb7")
-    
-    var region = CLBeaconRegion()
-    var manager = CLLocationManager()
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0")!, identifier: "MySmartDoor")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        manager.delegate = self
-        region = CLBeaconRegion(proximityUUID: uuidObj!, identifier: "MySmartDoor")
+        locationManager.delegate = self;
+        locationManager.startRangingBeaconsInRegion(region)
+        locationManager.requestAlwaysAuthorization()
+
     }
 
-    @IBAction func StartDetecting(sender: AnyObject) {
-        self.manager.requestAlwaysAuthorization()
-        self.manager.startMonitoringForRegion(self.region)
-        self.Found.text = "Starting Monitor"
-    }
-    
-    func locationManager(manager: CLLocationManager, didStartMonitoringForRegion region: CLRegion) {
-        self.Found.text = "Scanning..."
-        manager.startRangingBeaconsInRegion(region as! CLBeaconRegion) //testing line
-    }
-    
-    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        manager.startRangingBeaconsInRegion(region as! CLBeaconRegion)
-        self.Found.text = "Possible Match"
-    }
-    
-    func locationManager(manager: CLLocationManager, monitoringDidFailForRegion region: CLRegion?, withError error: NSError) {
-        self.Found.text = "Error :("
-        print(error)
-    }
-    
-    func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
-        manager.stopRangingBeaconsInRegion(region as! CLBeaconRegion)
-        reset()
-    }
-    
-    func reset(){
-        self.Found.text = "No"
-        self.UUID.text = "N/A"
-        self.Major.text = "N/A"
-        self.Minor.text = "N/A"
-        self.Accuracy.text = "N/A"
-        self.RSSI.text = "N/A"
-    }
-
-    
+ 
     func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
-        if(beacons.count == 0) { return }
         
-        let beacon = beacons.last
+        print(beacons[0])
+        updateInterface()
         
-        if (beacon!.proximity == CLProximity.Unknown) {
-            self.Distance.text = "Unknown Proximity"
-            reset()
-            return
-        } else if (beacon!.proximity == CLProximity.Immediate) {
-            self.Distance.text = "Immediate"
-        } else if (beacon!.proximity == CLProximity.Near) {
-            self.Distance.text = "Near"
-            //reset()
-        } else if (beacon!.proximity == CLProximity.Far) {
-            self.Distance.text = "Far"
-        }
-        self.Found.text = "Yes!"
-        self.UUID.text = beacon!.proximityUUID.UUIDString
-        self.Major.text = "\(beacon!.major)"
-        self.Minor.text = "\(beacon!.minor)"
-        self.Accuracy.text = "\(beacon!.accuracy)"
-        self.RSSI.text = "\(beacon!.rssi)"
+    }
+    
+    func updateInterface(){
+        //pass beacon in here and update everything
+        self.major.text = "0"
     }
 }
 
